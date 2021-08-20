f7635168f5d025503840a960f690665705ea00b0
imizu.yuki <student0107dd@gmail.com>
2021-08-20 05:35:17 +0000

    mailform model

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//入力ページ
+Route::get('/contact', 'ContactController@index')->name('contact.index');
+//確認ページ
+Route::post('/contact/confirm', 'ContactController@confirm')->name('contact.confirm');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//送信完了ページ
+Route::post('/contact/thanks', 'ContactController@send')->name('contact.send');
